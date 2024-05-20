1fa70ea52ce41b1f1741b71f91bdc9c8520d108c
Tadam <Tadam@gmail.com>
2024-05-20 11:51:45 +0700

    React

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+    style={{
+      flex: 1,
+      iustifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "lightblue",
+    }}>
+      <Text style={{ fontSize: 24, color: "firebrick"}}>
+        Hello,Inline Styles!
+      </Text>
+      <Image source={{uri: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" }}
+      width={64}
+      height={64}
+      />
       <StatusBar style="auto" />
     </View>
   );
